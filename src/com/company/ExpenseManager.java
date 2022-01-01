fa9e65d043f65dd5e3c884165369a23d304761c8
Sheetal Kabra <sheetal.kabra@eduvanz.in>
2022-01-01 21:14:53 +0530

    add io package

--- src/com/company/ExpenseManager.java
+++ src/com/company/ExpenseManager.java
@@ -7,8 +7,13 @@ import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 import com.company.model.Expense;
+import com.company.io.InputOutputJava;
 
 public class ExpenseManager {
+    private InputOutputJava io;
+    public ExpenseManager(){
+        io = new InputOutputJava();
+    }
 
     public enum menuItem {
         ADD_EXPENSE, DELETE_EXPENSE, SHOW_EXPENSE, UPDATE_EXPENSE, LEAVE_PROGRAM, NOT_VALID_ITEM;
@@ -26,12 +31,12 @@ public class ExpenseManager {
     };
 
     public static List<String> currentList = new ArrayList<>();
-    public static Date StringToDate(String dob) throws ParseException {
+    public Date StringToDate(String dob) throws ParseException {
         //Instantiating the SimpleDateFormat class
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
         //Parsing the given String to Date object
         Date date = formatter.parse(dob);
-        System.out.println("Date object value: " + date);
+        io.printMessageln("Date object value: " + date);
         return date;
     }
 
@@ -43,61 +48,61 @@ public class ExpenseManager {
                 case ADD_EXPENSE: addExpense();break;
                 case SHOW_EXPENSE: showExpense();break;
                 case DELETE_EXPENSE: deleteExpense();break;
-                case LEAVE_PROGRAM: System.out.println("Goodbye");break;
-                default: System.out.println("Enter valid option");
+                case LEAVE_PROGRAM: io.printMessageln("Goodbye");break;
+                default: io.printMessageln("Enter valid option");
             }
         }while(m != menuItem.LEAVE_PROGRAM);
     }
 
     public menuItem printMenu(){
-        System.out.println();
-        System.out.println("---------------------------------------------");
-        System.out.println("Main menu:");
-        System.out.println("1. Add an expense");
-        System.out.println("2. Show list of expenses");
-        System.out.println("3. Delete expense");
-        System.out.println("4. Leave a program");
-        System.out.println("---------------------------------------------");
-        System.out.print("Enter a choice:");
+        io.printMessageln("");
+        io.printMessageln("---------------------------------------------");
+        io.printMessageln("Main menu:");
+        io.printMessageln("1. Add an expense");
+        io.printMessageln("2. Show list of expenses");
+        io.printMessageln("3. Delete expense");
+        io.printMessageln("4. Leave a program");
+        io.printMessageln("---------------------------------------------");
+        io.printMessage("Enter a choice:");
         Scanner sc = new Scanner(System.in);
         int menuItemInput = sc.nextInt();
         return menuItem.getMenuItemFromInt(menuItemInput);
     }
 
     public void showExpense(){
-        System.out.println();
-        System.out.println("---------------------------------------------");
+        io.printMessageln("");
+        io.printMessageln("---------------------------------------------");
         if(!currentList.isEmpty()){
-            System.out.println("To-Do List:");
+            io.printMessageln("To-Do List:");
             int number=0;
             for(String item: currentList){
-                System.out.println(++number+ ". " +item);
+                io.printMessageln(++number+ ". " +item);
             }
         }else{
-            System.out.println("Expense list is empty!!!");
+            io.printMessageln("Expense list is empty!!!");
         }
 
-        System.out.println("---------------------------------------------");
+        io.printMessageln("---------------------------------------------");
     }
 
     public void addExpense() throws ParseException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the expense:\n===========");
-        System.out.print("Enter Date:");
+        io.printMessage("Enter the expense:\n===========");
+        io.printMessage("Enter the expense:\n===========");
+        io.printMessage("Enter Date:");
         String dateEntered = sc.next();
         Date convertDate = StringToDate(dateEntered);
-        System.out.println(new SimpleDateFormat("dd-MM-yyyy").format(convertDate));
-        System.out.print("Enter category:");
+        io.printMessage("Enter category:");
         String cat = sc.next();
-        System.out.print("Enter amount:");
+        io.printMessage("Enter amount:");
         double amount = sc.nextDouble();
-        System.out.print("Enter description:");
+        io.printMessage("Enter description:");
         String desc = sc.next();
-        System.out.print("Enter merchant:");
+        io.printMessage("Enter merchant:");
         String merchant = sc.next();
-        System.out.print("Enter payment method:");
+        io.printMessage("Enter payment method:");
         String paymentMethod = sc.next();
-        System.out.print("Enter tags:");
+        io.printMessage("Enter tags:");
         String tags = sc.next();
         Expense expense = new Expense();
         expense.setTheDate(convertDate);
@@ -109,21 +114,21 @@ public class ExpenseManager {
         expense.setTheTags(tags);
         String itemList = expense.getItem();
         currentList.add(itemList);
-        System.out.println("Item has been added into the expense list.");
-        System.out.println("========================");
+        io.printMessageln("Item has been added into the expense list.");
+        io.printMessageln("========================");
 
     }
 
     public boolean deleteExpense(){
         if(currentList.isEmpty()){
-            System.out.println("Sorry, Expense list is empty");
+            io.printMessageln("Sorry, Expense list is empty");
             return false;
         }
-        System.out.println("Enter expense number you want to delete");
+        io.printMessageln("Enter expense number you want to delete");
         Scanner sc = new Scanner(System.in);
         int indexTodelete = sc.nextInt();
         currentList.remove(indexTodelete-1);
-        System.out.println("Item "+indexTodelete+" has been deleted from the expense list.");
+        io.printMessageln("Item "+indexTodelete+" has been deleted from the expense list.");
         return true;
     }
 
---
+++ src/com/company/io/InputOutputJava.java
@@ -0,0 +1,10 @@
+package com.company.io;
+
+public class InputOutputJava {
+    public void printMessage(String messgae){
+        System.out.println(messgae);
+    }
+    public void printMessageln(String messgae){
+        System.out.println(messgae);
+    }
+}
